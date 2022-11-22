4ff4982f14faee62d95eb701263f7ba55d4f2612
Jetro De Leon <jdeleon5@rrc.ca>
2022-11-21 18:20:21 -0600

    Alien Class Finalization
    Added comments to the Alien class and move the implementation for the .h file to the .cpp file.

--- src/Alien.cpp
+++ src/Alien.cpp
@@ -1,1 +1,85 @@
 ﻿#include "Alien.h"
+
+Alien::Alien(const Coordinate& coordinate, const Type& type): myCoordinate{ coordinate }, myType{ type } {
+	loadSprite();
+	collision = ofRectangle{ static_cast<float>(myCoordinate.x), static_cast<float>(myCoordinate.y), myAvatar.getWidth(), myAvatar.getHeight() };
+	isAlienAlive = true;
+	myDestructionAudio.load("Audio/alienShipExplosion.mp3");
+}
+
+void Alien::draw() const {
+	myAvatar.draw(myCoordinate.x, myCoordinate.y);
+}
+
+void Alien::update(Coordinate changeInCoordinate) {
+	myCoordinate.x += changeInCoordinate.x;
+	myCoordinate.y += changeInCoordinate.y;
+	if (isAlienAlive) {
+		if (ofGetRectMode() == OF_RECTMODE_CENTER) {
+			// ofRectangle intersect() doesn't seem to work with OF_RECTMODE_CENTER
+			// offset collision coordinates by half the avatar's width and height
+			collision.setX(myCoordinate.x - myAvatar.getWidth() / 2);
+			collision.setY(myCoordinate.y - myAvatar.getHeight() / 2);
+		}
+		else {
+			collision.setX(myCoordinate.x);
+			collision.setY(myCoordinate.y);
+		}
+	}
+	else {
+		// can't delete collision, so hide it far away
+		collision.setX(-99);
+		collision.setY(-99);
+	}
+}
+
+int Alien::value() const {
+	int alienValue = 0;
+	switch (myType) {
+	case Type::bottom:
+		alienValue = 10;
+		break;
+	case Type::middle:
+		alienValue = 20;
+		break;
+	case Type::top:
+		alienValue = 30;
+		break;
+	}
+	return alienValue;
+}
+
+void Alien::destroy() {
+	isAlienAlive = false;
+	myDestructionAudio.play();
+}
+
+bool Alien::isAlive() const {
+	return isAlienAlive;
+}
+
+Coordinate Alien::getWeaponCoordinate() const {
+	// weapon is located at the middle front of the ship
+	return Coordinate{ myCoordinate.x, myCoordinate.y + myAvatar.getHeight() / 2 };
+}
+
+bool Alien::isOnBoundary(double leftBoundary, double rightBoundary) const {
+	// check if the alien has reached the side boundaries
+	return (myCoordinate.x <= leftBoundary || myCoordinate.x >= rightBoundary) && isAlienAlive;
+}
+
+void Alien::loadSprite() {
+	// load alien avatar
+	switch(myType) {
+	case Type::top:
+		myAvatar.load("Assets/yellowAlienShip.png");
+		break;
+	case Type::middle:
+		myAvatar.load("Assets/purpleAlienShip.png");
+		break;
+	case Type::bottom:
+		myAvatar.load("Assets/redAlienShip.png");
+		break;
+	}
+	myAvatar.resize(40, 40);
+}
--- src/Alien.h
+++ src/Alien.h
@@ -10,91 +10,21 @@ public:
 	enum class Type{ bottom, middle, top };
 	ofRectangle collision;
 private:
-	ofImage myAvatar;
 	Coordinate myCoordinate;
 	Type myType;
-	bool isAlienAlive;
+	ofImage myAvatar;
 	ofSoundPlayer myDestructionAudio;
+	bool isAlienAlive;
 public:
-	Alien(const Coordinate& coordinate, const Type& type)
-		: myCoordinate{ coordinate }, myType{ type } {
-		setSprite();
-		collision = ofRectangle{ static_cast<float>(myCoordinate.x), static_cast<float>(myCoordinate.y), myAvatar.getWidth(), myAvatar.getHeight() };
-		isAlienAlive = true;
-		myDestructionAudio.load("Audio/alienShipExplosion.mp3");
-	}
+	Alien(const Coordinate& coordinate, const Type& type);
 
-	void draw() const {
-		myAvatar.draw(myCoordinate.x, myCoordinate.y);
-	}
-
-	int value() const {
-		int alienValue = 0;
-		switch (myType) {
-		case Type::bottom:
-			alienValue = 10;
-			break;
-		case Type::middle:
-			alienValue = 20;
-			break;
-		case Type::top:
-			alienValue = 30;
-			break;
-		}
-		return alienValue;
-	}
-
-	Coordinate getWeaponCoordinate() const {
-		// weapon is located at the middle front of the ship
-		return Coordinate{ myCoordinate.x, myCoordinate.y + myAvatar.getHeight() / 2 };
-	}
-
-	void update(Coordinate changeInCoordinate) {
-		myCoordinate.x += changeInCoordinate.x;
-		myCoordinate.y += changeInCoordinate.y;
-		if (isAlienAlive) {
-			if (ofGetRectMode() == OF_RECTMODE_CENTER) {
-				// ofRectangle intersect() doesn't seem to work with OF_RECTMODE_CENTER
-				collision.setX(myCoordinate.x - myAvatar.getWidth() / 2);
-				collision.setY(myCoordinate.y - myAvatar.getHeight() / 2);
-			}
-			else {
-				collision.setX(myCoordinate.x);
-				collision.setY(myCoordinate.y);
-			}
-		} else {
-			collision.setX(-99);
-			collision.setY(-99);
-		}
-
-	}
-
-	bool isOnBoundary(double leftBoundary, double rightBoundary) const {
-		return (myCoordinate.x <= (leftBoundary + 5) || myCoordinate.x >= (rightBoundary - 5)) && isAlienAlive;
-	}
-
-	void destroy() {
-		isAlienAlive = false;
-		myDestructionAudio.play();
-	}
-
-	bool isAlive() const {
-		return isAlienAlive;
-	}
-
+	void draw() const;
+	void update(Coordinate changeInCoordinate);
+	int value() const;
+	void destroy();
+	bool isAlive() const;
+	Coordinate getWeaponCoordinate() const;
+	bool isOnBoundary(double leftBoundary, double rightBoundary) const;
 private:
-	void setSprite() {
-		switch(myType) {
-		case Type::top:
-			myAvatar.load("Assets/yellowAlienShip.png");
-			break;
-		case Type::middle:
-			myAvatar.load("Assets/purpleAlienShip.png");
-			break;
-		case Type::bottom:
-			myAvatar.load("Assets/redAlienShip.png");
-			break;
-		}
-		myAvatar.resize(40, 40);
-	}
+	void loadSprite();
 };
